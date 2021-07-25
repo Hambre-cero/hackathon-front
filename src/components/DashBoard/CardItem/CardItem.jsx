846228de4b34082123c21ca804a4ed3e3d87aadc
Adrian Alexander <adrianmauricio337@gmail.com>
2021-07-25 06:35:15 -0500

    create CardItem component from dashboard page

--- src/components/DashBoard/CardItem/CardItem.jsx
+++ src/components/DashBoard/CardItem/CardItem.jsx
@@ -1,9 +1,31 @@
-const CardItem = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import LocationIcon from '../../../assets/icons/location.svg'
+import '../../css/CardItem.css'
+
+const CardItem = ({ image, title, price, category }) => {
+  return (
+    <div key="1" className="CardItem">
+      <img className="CardItem__photo" src={image} alt="Imagen de fondo" />
+      <div className="CardItem__description">
+        <p className="CardItem__description-info--2">{title}</p>
+        <p className="CardItem__description-info--1">{price}</p>
+        <p className="CardItem__description-info--2">{category}</p>
+      </div>
+      <div className="CardItem__location">
+        <Link className="btn--link" to="/">
+          Find Me
+          <img className="icon" src={LocationIcon} alt="location icon" />
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default CardItem
+CardItem.propTypes = {
+  image: PropTypes.node.isRequired,
+  title: PropTypes.node.isRequired,
+  price: PropTypes.node.isRequired,
+  category: PropTypes.node.isRequired
+}
--- src/components/DashBoard/Home/Home.jsx
+++ src/components/DashBoard/Home/Home.jsx
@@ -1,5 +1,39 @@
+import { useState, useEffect } from 'react'
+import CardItem from '../CardItem/CardItem'
+import '../../css/Home.css'
+
 const Home = () => {
-  return <div>Home</div>
+  const [state, setState] = useState({ loading: true, data: [] })
+
+  const fetchData = async () => {
+    const dat = await fetch('https://fakestoreapi.com/products')
+    const datjson = await dat.json()
+
+    setState({ loading: false, data: datjson })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (state.loading) {
+    return 'Cargando'
+  }
+
+  return (
+    <ul className="gallery">
+      {state.data.map((item) => {
+        return (
+          <CardItem
+            image={item.image}
+            title={item.title}
+            price={item.price}
+            category={item.category}
+          />
+        )
+      })}
+    </ul>
+  )
 }
 
 export default Home
