6c489449b60d0ef6bd333e41d2851ecbcc7ed217
Nicolas-alt <a10100105555@gmail.com>
2021-07-25 15:25:49 -0500

    fix: loader ui

--- src/pages/DashBoard/DashBoard.jsx
+++ src/pages/DashBoard/DashBoard.jsx
@@ -10,7 +10,7 @@ const DashBoard = ({ children }) => {
         <div className="flex flex-col w-full pl-0 md:p-4 md:space-y-4">
           <Header />
           <div className="h-screen pt-2 pb-24 pl-2 pr-2 overflow-auto md:pt-0 md:pr-0 md:pl-0">
-            <div className="flex flex-col flex-wrap sm:flex-row ">
+            <div className="flex flex-col flex-wrap items-center justify-center h-full sm:flex-row">
               {children}
             </div>
           </div>
