7b00b9830a5b0be2decb7a2ad8080febde075967
Nicolas-alt <a10100105555@gmail.com>
2021-07-24 20:05:34 -0500

    fix: dashboard links

--- src/routers/DashBoardRouter.jsx
+++ src/routers/DashBoardRouter.jsx
@@ -1,5 +1,6 @@
 import { Redirect, Route, Switch } from 'react-router-dom'
 import Home from '../components/DashBoard/Home/Home'
+import Settings from '../components/DashBoard/Settings/Settings'
 import DashBoard from '../pages/DashBoard/DashBoard'
 
 const DashBoardRouter = () => {
@@ -7,6 +8,7 @@ const DashBoardRouter = () => {
     <DashBoard>
       <Switch>
         <Route exact path="/dashboard/home" component={Home} />
+        <Route exact path="/dashboard/settings" component={Settings} />
         <Redirect to="/dashboard/home" />
       </Switch>
     </DashBoard>
