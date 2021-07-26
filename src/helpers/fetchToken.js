08cc55a30376e0fa69dbd7546096e6fd4ad0ce76
Nicolas-alt <a10100105555@gmail.com>
2021-07-25 19:05:20 -0500

    update: tokenFetch docs

--- src/helpers/fetchToken.js
+++ src/helpers/fetchToken.js
@@ -2,6 +2,14 @@ import toast from 'react-hot-toast'
 
 const baseUrl = process.env.REACT_APP_URL_BASE
 
+/**
+ * 
+ * @param {string} endpoint Por ejemplo 'users/me' NO SE PONE / antes de el texto '/users/me' NO
+ * @param {any} data Data que quiera enviar, preferiblemente un objeto
+ * @param {string} method Por default es Get, pero puede enviar cualquier otro metodo.
+ * @returns Depende del endpoint, puede ser un objeto o nada.
+ */
+
 const fetchToken = async (endpoint, data, method = 'GET') => {
   const token = localStorage.getItem('tokenEdonate') || ''
   const urlFetch = `${baseUrl}/${endpoint}`
